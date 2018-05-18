d7173349e3e6850b686aef65a560e896ba9b6c82
tanacchi <q111026d@mail.kyutech.jp>
2018-05-19 01:07:03 +0900

    [WIP]Add othello.c

---
+++ othello.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int field[8][8]={0};
+int flag = 0;
+
+void show(){
+  int a,b;
+  
+  printf("\n 1 2 3 4 5 6 7 8 \n");
+  for(a = 1; a < 9; a++){
+	printf("%d",i);
+	for(b = 1; b < 9; b++){
+	  switch(field[a][b]){
+	  case 0:
+		printf("-");
+		break;
+	  case 2:
+		print("O");
+		break;
+	  default:
+		printf("NN");
+	  }
+	}
+	printf("\n");
+  }
+}
+
+int check(int ro,int co,int a,int b,int turn){
+  if(field[ro][co]==turn){
+	flag=1;
+	return turn;
+  }else if()
+	
